b701f94a3365b1f3b92ef954798a2614aa677a1c
TDanyStark <docente.sistemas.daniel@gmail.com>
2022-09-17 16:08:49 -0500

    react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import Bootstrap from './components/Bootstrap';
+// import Bootstrap from './components/Bootstrap';
 import './App.css';
-import Bulma from './components/Bootstrap';
+// import Bulma from './components/Bootstrap';
+import ReactBootstrap from './components/ReactBootstrap';
 
 function App() {
   return (
     <div>
       <h1>Frameworks css en react</h1>
       {/* <Bootstrap /> */}
-      <Bulma />
+      {/* <Bulma /> */}
+      <ReactBootstrap/>
     </div>
   );
 }
